3a5c2dfcf2d13c8cc8f23a093b76db6b0fa13fce
Sana-Farasat <naz62354@gmail.com>
2025-04-23 03:46:25 +0500

    Portfolio styled by tailwand css

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-function Cli_Projects(){
-    return(
-        <main>
-            <section>
+function Cli_Projects() {
+  return (
+    <main>
+      <section>
         <div className="text-center text-xl md:text-3xl text-white mt-4">
           <h2 className=" text-xl md:text-3xl sm:text-[40px] font-bold">
             CLI Based Projects Here!!
@@ -15,100 +15,110 @@ function Cli_Projects(){
         </div>
 
         <div className=" flex flex-col items-center sm:flex sm:flex-row justify-around mt-4 ">
-          <motion.div
-            initial={{ opacity: 0, x: -100 }} //Left to Right
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.6 }}
-            viewport={{ once: true }}
-            className="bg-green-200 shadow-lg h-auto max-w-sm mb-3 mx-2 flex justify-around p-auto rounded-lg text-green-950 transition-transform duration-75 scale-100 hover:scale-105 ease-in-out"
-          >
-            <p className="p-1 text-sm">
-              The Number Guessing Game is a simple yet engaging command-line
-              interface (CLI) game that challenges players to guess a randomly
-              generated number within a specific range. This project
-              demonstrates my skills in programming logic, user interaction, and
-              game design.
-            </p>
-            <Image
-              src="/ngg.png"
-              alt="Number Guessing Game"
-              height={1}
-              width={100}
-            />
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, x: 100 }} //Right to Left
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.6 }}
-            viewport={{ once: true }}
-            className="bg-red-100 shadow-lg h-auto max-w-sm mb-3 mx-2  flex justify-around p-auto rounded-lg text-red-950 transition-transform duration-75 scale-100 hover:scale-105 ease-in-out"
-          >
-            <p className="p-1 text-sm">
-              The ATM Simulator is a command-line interface (CLI) application
-              that replicates the functionality of a real ATM. This project
-              allows users to perform basic banking transactions such as
-              checking balances, withdrawing cash, and depositing funds,
-              demonstrating my skills in application logic and user interaction.
-            </p>
+          <div className="overflow-hidden">
+            <motion.div
+              initial={{ opacity: 0, x: -100 }} //Left to Right
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.6 }}
+              viewport={{ once: true }}
+              className="bg-green-200 shadow-lg h-auto max-w-sm mb-3 mx-2 flex justify-around p-auto rounded-lg text-green-950 transition-transform duration-75 scale-100 hover:scale-105 ease-in-out"
+            >
+              <p className="p-1 text-sm">
+                The Number Guessing Game is a simple yet engaging command-line
+                interface (CLI) game that challenges players to guess a randomly
+                generated number within a specific range. This project
+                demonstrates my skills in programming logic, user interaction,
+                and game design.
+              </p>
+              <Image
+                src="/ngg.png"
+                alt="Number Guessing Game"
+                height={1}
+                width={100}
+              />
+            </motion.div>
+          </div>
+          <div className="overflow-hidden">
+            <motion.div
+              initial={{ opacity: 0, x: 100 }} //Right to Left
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.6 }}
+              viewport={{ once: true }}
+              className="bg-red-100 shadow-lg h-auto max-w-sm mb-3 mx-2  flex justify-around p-auto rounded-lg text-red-950 transition-transform duration-75 scale-100 hover:scale-105 ease-in-out"
+            >
+              <p className="p-1 text-sm">
+                The ATM Simulator is a command-line interface (CLI) application
+                that replicates the functionality of a real ATM. This project
+                allows users to perform basic banking transactions such as
+                checking balances, withdrawing cash, and depositing funds,
+                demonstrating my skills in application logic and user
+                interaction.
+              </p>
 
-            <Image
-              src="/atm.png"
-              alt="ATM"
-              height={0}
-              width={120}
-              className="rounded-lg"
-            />
-          </motion.div>
+              <Image
+                src="/atm.png"
+                alt="ATM"
+                height={0}
+                width={120}
+                className="rounded-lg"
+              />
+            </motion.div>
+          </div>
         </div>
 
         <div className=" flex flex-col items-center sm:flex sm:flex-row justify-around mt-4 ">
-          <motion.div
-            initial={{ opacity: 0, x: -100 }} //Left to Right
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.6 }}
-            viewport={{ once: true }}
-            className="bg-gray-200 shadow-lg h-auto max-w-sm mb-3 mx-2 flex justify-around p-auto rounded-lg text-gray-950 transition-transform duration-75 scale-100 hover:scale-105 ease-in-out"
-          >
-            <p className="p-1 text-sm">
-              The CLI Calculator is a command-line interface application that
-              performs basic arithmetic operations. This project showcases my
-              ability to implement mathematical logic and user interaction in a
-              console environment.
-            </p>
-            <Image
-              src="/cal.png"
-              alt="Calculator"
-              height={0}
-              width={120}
-              className="rounded-lg"
-            />
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, x: 100 }} //Right to Left
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.6 }}
-            viewport={{ once: true }}
-            className="bg-yellow-200 shadow-lg h-auto max-w-sm mb-3 mx-2 flex justify-around p-auto rounded-lg text-yellow-950 transition-transform duration-75 scale-100 hover:scale-105 ease-in-out"
-          >
-            <p className="p-1 text-sm">
-              The CLI Currency Converter is a command-line interface application
-              that allows users to convert amounts between different currencies.
-              This project highlights my skills in API integration, data
-              handling, and user interaction in a console environment.
-            </p>
+          <div className="overflow-hidden">
+            <motion.div
+              initial={{ opacity: 0, x: -100 }} //Left to Right
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.6 }}
+              viewport={{ once: true }}
+              className="bg-gray-200 shadow-lg h-auto max-w-sm mb-3 mx-2 flex justify-around p-auto rounded-lg text-gray-950 transition-transform duration-75 scale-100 hover:scale-105 ease-in-out"
+            >
+              <p className="p-1 text-sm">
+                The CLI Calculator is a command-line interface application that
+                performs basic arithmetic operations. This project showcases my
+                ability to implement mathematical logic and user interaction in
+                a console environment.
+              </p>
+              <Image
+                src="/cal.png"
+                alt="Calculator"
+                height={0}
+                width={120}
+                className="rounded-lg"
+              />
+            </motion.div>
+          </div>
+          <div className="overflow-hidden">
+            <motion.div
+              initial={{ opacity: 0, x: 100 }} //Right to Left
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.6 }}
+              viewport={{ once: true }}
+              className="bg-yellow-200 shadow-lg h-auto max-w-sm mb-3 mx-2 flex justify-around p-auto rounded-lg text-yellow-950 transition-transform duration-75 scale-100 hover:scale-105 ease-in-out"
+            >
+              <p className="p-1 text-sm">
+                The CLI Currency Converter is a command-line interface
+                application that allows users to convert amounts between
+                different currencies. This project highlights my skills in API
+                integration, data handling, and user interaction in a console
+                environment.
+              </p>
 
-            <Image
-              src="/curr.png"
-              alt="Currency Convertor"
-              height={0}
-              width={120}
-              className="rounded-lg"
-            />
-          </motion.div>
+              <Image
+                src="/curr.png"
+                alt="Currency Convertor"
+                height={0}
+                width={120}
+                className="rounded-lg"
+              />
+            </motion.div>
+          </div>
         </div>
       </section>
-        </main>
-    )
+    </main>
+  );
 }
 
 export default Cli_Projects;
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { FaCss3Alt, FaReact } from "react-icons/fa";
 import { IoLogoHtml5, IoLogoJavascript } from "react-icons/io";
@@ -47,155 +47,169 @@ function Skills() {
       </h2>
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center gap-7 max-w-5xl mx-auto overflow-visible ">
         {/* ---------------------Html------------------- */}
-        <motion.div
-          initial={{ opacity: 0, x: -100 }} //Left to Right
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
-        >
-          {" "}
-          <IoLogoHtml5 className="text-red-500 h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
-          <h2 className="text-red-600">HTML</h2>
-        </motion.div>
-
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: -100 }} //Left to Right
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
+          >
+            {" "}
+            <IoLogoHtml5 className="text-red-500 h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
+            <h2 className="text-red-600">HTML</h2>
+          </motion.div>
+        </div>
         {/* ---------------------CSS------------------- */}
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: 100 }} //Right to Left
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
+          >
+            <FaCss3Alt className="text-blue-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
 
-        <motion.div
-          initial={{ opacity: 0, x: 100 }} //Right to Left
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
-        >
-          <FaCss3Alt className="text-blue-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
-
-          <h2 className="text-blue-600">CSS</h2>
-        </motion.div>
-
+            <h2 className="text-blue-600">CSS</h2>
+          </motion.div>
+        </div>
         {/* ---------------------JavaScript------------------- */}
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: 100 }} //Right to Left
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
+          >
+            <IoLogoJavascript className="text-yellow-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
 
-        <motion.div
-          initial={{ opacity: 0, x: 100 }} //Right to Left
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
-        >
-          <IoLogoJavascript className="text-yellow-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
-
-          <h2 className="text-yellow-600">JavaScript</h2>
-        </motion.div>
-
+            <h2 className="text-yellow-600">JavaScript</h2>
+          </motion.div>
+        </div>
         {/* ---------------------TypeScript------------------- */}
-        <motion.div
-          initial={{ opacity: 0, x: -100 }} //Left to Right
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110 "
-        >
-          <SiTypescript className="text-blue-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: -100 }} //Left to Right
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110 "
+          >
+            <SiTypescript className="text-blue-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
 
-          <h2 className="text-blue-600">TypeScript</h2>
-        </motion.div>
+            <h2 className="text-blue-600">TypeScript</h2>
+          </motion.div>
+        </div>
         {/* ---------------------React------------------- */}
-        <motion.div
-          initial={{ opacity: 0, x: -100 }} //Left to Right
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
-        >
-          <FaReact className="text-pink-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: -100 }} //Left to Right
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
+          >
+            <FaReact className="text-pink-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
 
-          <h2 className="text-pink-600">React</h2>
-        </motion.div>
+            <h2 className="text-pink-600">React</h2>
+          </motion.div>
+        </div>
 
         {/* ---------------------Nextjs------------------- */}
-        <motion.div
-          initial={{ opacity: 0, x: 100 }} //Right to Left
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
-        >
-          <RiNextjsFill className="text-black  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: 100 }} //Right to Left
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
+          >
+            <RiNextjsFill className="text-black  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
 
-          <h2 className="text-black">Nextjs</h2>
-        </motion.div>
-
+            <h2 className="text-black">Nextjs</h2>
+          </motion.div>
+        </div>
         {/* ---------------------Python------------------- */}
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: -100 }} //Left to Right
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
+          >
+            <SiPython className="text-blue-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
 
-        <motion.div
-          initial={{ opacity: 0, x: -100 }} //Left to Right
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
-        >
-          <SiPython className="text-blue-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
-
-          <h2 className="text-blue-600">Python</h2>
-        </motion.div>
-
+            <h2 className="text-blue-600">Python</h2>
+          </motion.div>
+        </div>
         {/* ---------------------Streamlit------------------- */}
-        <motion.div
-          initial={{ opacity: 0, x: 100 }} //Right to Left
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
-        >
-          <SiStreamlit className="text-red-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
-          <h2 className="text-red-600">Streamlit</h2>
-        </motion.div>
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: 100 }} //Right to Left
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
+          >
+            <SiStreamlit className="text-red-500  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
+            <h2 className="text-red-600">Streamlit</h2>
+          </motion.div>
+        </div>
         {/* ---------------------Vercel------------------- */}
-        <motion.div
-          initial={{ opacity: 0, x: 100 }} //Right to Left
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
-        >
-          <SiVercel className="text-white  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
-          <h2 className="text-black-600">Vercel</h2>
-        </motion.div>
-
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: 100 }} //Right to Left
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
+          >
+            <SiVercel className="text-white  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
+            <h2 className="text-black-600">Vercel</h2>
+          </motion.div>
+        </div>
         {/* ---------------------Sanity------------------- */}
-        <motion.div
-          initial={{ opacity: 0, x: -100 }} //Left to Right
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
-        >
-          <SiSanity className="text-red-600  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
-          <h2 className="text-red-600">Sanity</h2>
-        </motion.div>
-
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: -100 }} //Left to Right
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
+          >
+            <SiSanity className="text-red-600  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
+            <h2 className="text-red-600">Sanity</h2>
+          </motion.div>
+        </div>
         {/* ---------------------Npm------------------- */}
-        <motion.div
-          initial={{ opacity: 0, x: -100 }} //Left to Right
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
-        >
-          <SiNpm className="text-red-600  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
-          <h2 className="text-red-600">NPM</h2>
-        </motion.div>
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: -100 }} //Left to Right
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
+          >
+            <SiNpm className="text-red-600  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
+            <h2 className="text-red-600">NPM</h2>
+          </motion.div>
+        </div>
         {/* ---------------------Daisy Ui------------------- */}
-        <motion.div
-          initial={{ opacity: 0, x: 100 }} //Right to Left
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
-        >
-          <SiDaisyui className="text-red-600  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
-          <h2 className="text-red-600">Daisy Ui</h2>
-        </motion.div>
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, x: 100 }} //Right to Left
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className="rounded-3xl w-72 h-48 flex flex-col justify-center items-center text-3xl text-white mt-10 bg-gradient-to-t  from-gray-100 via-gray-800 to-gray-950 transition-transform duration-300 hover:scale-110"
+          >
+            <SiDaisyui className="text-red-600  h-24 w-24 md:h-28 md:w-28 transition-transform duration-300 hover:scale-110" />
+            <h2 className="text-red-600">Daisy Ui</h2>
+          </motion.div>
+        </div>
         <div></div>
         <br />
         <br />
